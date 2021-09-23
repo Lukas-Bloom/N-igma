9318cad4e4c46523d8d0098bba1ad16f28757b50
Hugo Granath <72066319+mrHugolo@users.noreply.github.com>
2021-09-23 09:22:06 +0200

    removed double code

--- main.js
+++ main.js
@@ -81,50 +81,33 @@ scene("game", () => {
     pos(-200, 0),
     area(),
     scale(2),
-    color(0,0,240),
+    color(0, 0, 240),
     // makes it fall to gravity and jumpable
     body(),
     // the custom component we defined above
     origin("bot"),
   ]);
 
+  const players = [player1, player2]
+
+
   // action() runs every frame
-  player1.action(() => {
+  players[playerNumber - 1].action(() => {
     // center camera to player
-    camPos(player1.pos);
+    camPos(players[playerNumber - 1].pos);
     // check fall death
-    if (player1.pos.y >= FALL_DEATH) {
+    if (players[playerNumber - 1].pos.y >= FALL_DEATH) {
       go("lose");
     }
   });
 
-  player2.action(() => {
-    // center camera to player
-    camPos(player2.pos);
-    // check fall death
-    if (player2.pos.y >= FALL_DEATH) {
-      go("lose");
-    }
+  keyDown("left", () => {
+    players[playerNumber - 1].move(-MOVE_SPEED, 0);
   });
 
-
-  if (playerNumber === 1) {
-    keyDown("left", () => {
-      player1.move(-MOVE_SPEED, 0);
-    });
-
-    keyDown("right", () => {
-      player1.move(MOVE_SPEED, 0);
-    });
-  } else if (playerNumber === 2) {
-    keyDown("left", () => {
-      player2.move(-MOVE_SPEED, 0);
-    });
-
-    keyDown("right", () => {
-      player2.move(MOVE_SPEED, 0);
-    });
-  }
+  keyDown("right", () => {
+    players[playerNumber - 1].move(MOVE_SPEED, 0);
+  });
 
 });
 
