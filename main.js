3157d1f789c992e28b3b96c530111358853127e6
Kristian <the.dyze@gmail.com>
2021-09-23 11:51:39 +0200

    added testlevel, sprites

--- main.js
+++ main.js
@@ -47,27 +47,85 @@ kaboom({
 const MOVE_SPEED = 480;
 const FALL_DEATH = 2400;
 
+//load assets
 loadSprite("bean", "sprites/bean.png");
+loadSprite("grass", "sprites/grass.png");
+loadSprite("grass_l", "sprites/grass_l.png");
+loadSprite("grass_r", "sprites/grass_r.png");
+loadSprite("grass_l_top", "sprites/grass_l_top.png");
+loadSprite("grass_r_top", "sprites/grass_r_top.png");
+loadSprite("box", "sprites/box.png");
+loadSprite("spikes", "sprites/spikes.png");
 
-scene("game", () => {
+  const LEVELS = [
+    [
+    "                                                          ",
+    "                             ####           L=============",
+    "                                            1             ",
+    "                        #            L======              ",
+    "                                     1                    ",
+    "              L===R                  1                    ",
+    "              1   2                  1                    ",
+    "              1   2       ^^^^^      1                    ",
+    "==============     ==================                     ",
+    ],
+  ]
+
+  const levelConf =  {
+    // define the size of each block
+    width: 16,
+    height: 16,
+    // define what each symbol means
+    "=": () => [
+      sprite("grass"),
+      area(),
+      solid(),
+    ],
+    "1": () => [
+      sprite("grass_l"),
+      area(),
+      solid(),
+    ],
+    "2": () => [
+      sprite("grass_r"),
+      area(),
+      solid(),
+    ],
+    "L": () => [
+      sprite("grass_l_top"),
+      area(),
+      solid(),
+    ],
+    "R": () => [
+      sprite("grass_r_top"),
+      area(),
+      solid(),
+    ],
+    "#": () => [
+      sprite("box"),
+      area(),
+      solid(),
+    ],
+    "^": () => [
+      sprite("spikes"),
+      area(),
+      solid(),
+    ],
+    
+};
+  
+scene("game", ({ levelId } = { levelId: 0}) => {
+
   gravity(3200);
 
   // add level to scene
+  const level = addLevel(LEVELS[levelId ?? 0], levelConf);
 
-  add([
-    rect(width(), 100),
-    outline(4),
-    pos(-500, height()),
-    origin("botleft"),
-    area(),
-    solid(),
-    color(127, 200, 255),
-  ]);
 
   // define player object
   const player1 = add([
     sprite("bean"),
-    pos(-400, 0),
+    pos(50, 0),
     area(),
     scale(2),
     // makes it fall to gravity and jumpable
@@ -78,7 +136,7 @@ scene("game", () => {
 
   const player2 = add([
     sprite("bean"),
-    pos(-200, 0),
+    pos(100, 0),
     area(),
     scale(2),
     color(0, 0, 240),
@@ -107,6 +165,10 @@ scene("game", () => {
 
   keyDown("right", () => {
     players[playerNumber - 1].move(MOVE_SPEED, 0);
+  });
+
+  keyPress("space", () => {
+    players[playerNumber - 1].jump();
   });
 
 });
