7168b7a1f30550bf4c16898a867b65a4c9c37fb3
lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>
2021-10-06 13:55:32 +0200

    fixed display toggle on html divs.

--- gameCreation.js
+++ gameCreation.js
@@ -10,6 +10,8 @@ export const createGame = () => {
 
   newGameBtn.addEventListener("click", newGame);
   joinGameBtn.addEventListener("click", joinGame);
+  
+  
  
   
   function newGame() {
@@ -17,6 +19,10 @@ export const createGame = () => {
     if (document.getElementById("newGameButton")) {
       document.getElementById("newGameButton").remove();
       document.getElementById("joinGameButton").remove();
+      toggleDisplay(document.getElementById("mainMenu"));
+      toggleDisplay(document.getElementById("gamecodeDiv"));
+  }
+
     }
     socket.on("joinCode", (c) => {
       console.log(c);
@@ -35,6 +41,8 @@ export const createGame = () => {
     if (document.getElementById("newGameButton")) {
       document.getElementById("newGameButton").remove();
       document.getElementById("joinGameButton").remove();
+      toggleDisplay(document.getElementById("mainMenu"));
+      toggleDisplay(document.getElementById("gamecodeDiv"));
     }
 
     socket.on("init2", (pNumber) => {
@@ -44,4 +52,11 @@ export const createGame = () => {
       game(p, otherPlayer, levelIndex);
     });
   }
+  function toggleDisplay(obj) {
+    var x = obj
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      } else {
+      x.style.display = "none";
+      }
 };
