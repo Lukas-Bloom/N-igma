9dcd6e4d0c10003be40f533209ac6246e982f094
lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>
2021-10-06 18:43:56 +0200

    works overall, hides what you want

--- gameCreation.js
+++ gameCreation.js
@@ -24,39 +24,39 @@ export const createGame = () => {
   let joinCode;
   let newGameBtn;
   let joinGameBtn;
-  let joinGameBtn2
   let mainMenuDiv;
   let gamecodeDiv;
 
   if (!playerNumber) {
+    mainMenuDiv = document.getElementById("mainMenuDiv");
+    gamecodeDiv = document.getElementById("gamecodeDiv");
     codeInput = document.getElementById("codeInput");
     joinCode = document.getElementById("joinCode");
     newGameBtn = document.getElementById("newGameButton");
     joinGameBtn = document.getElementById("joinGameButton");
-    joinGameBtn2 = document.getElementById("joinGameButton2")
-    joinGameBtn.addEventListener("click", toggleDisplay(document.getElementById("mainMenuDiv")), toggleDisplay(document.getElementById("gamecodeDiv")));
-    joinGameBtn2.addEventListener("click", joinGame);
-    newGameBtn.addEventListener("click", newGame, toggleDisplay(document.getElementById("mainMenuDiv")), toggleDisplay(document.getElementById("gamecodeDiv")));
-    mainMenuDiv = document.getElementById("mainMenuDiv");
-    gamecodeDiv = document.getElementById("gamecodeDiv");
+    joinGameBtn.addEventListener("click", joinGame, toggleDisplay(document.getElementById("mainMenuDiv")),toggleDisplay(document.getElementById("gamecodeDiv")));
+    newGameBtn.addEventListener("click", newGame, toggleDisplay(document.getElementById("mainMenuDiv")),toggleDisplay(document.getElementById("gamecodeDiv")));
+    
   }
 
   if (playerNumber === 1) newGame();
 
   function newGame() {
     socket.emit("startGame", getData("roooom"));
-
+ 
     socket.on("joinCode", (c) => {
       setData("roooom", c);
-      if (joinCode) joinCode.append("Share this code: ", c);
+      if (joinCode) joinCode.append("Gamecode: ", c);
     });
     socket.on("init", () => {
       const otherPlayer = 2;
       const p = 1;
       let playerNumber = 1;
       setData("playerNumber", playerNumber);
-
+      toggleDisplay(document.getElementById("mainMenuDiv"));
+      toggleDisplay(document.getElementById("gamecodeDiv"));
       game(p, otherPlayer);
+      
     });
   }
 
@@ -74,15 +74,13 @@ export const createGame = () => {
     });
 
     socket.on("init2", () => {
-      if (document.getElementById("joinGameButton"))
-        document.getElementById("joinGameButton").remove();
-      if (document.getElementById("codeInput"))
-        document.getElementById("codeInput").remove();
       const otherPlayer = 1;
       const p = 2;
       let playerNumber = 2;
       setData("playerNumber", playerNumber);
       game(p, otherPlayer);
+      toggleDisplay(document.getElementById("mainMenuDiv"));
+      toggleDisplay(document.getElementById("gamecodeDiv"));
     });
   }
 
